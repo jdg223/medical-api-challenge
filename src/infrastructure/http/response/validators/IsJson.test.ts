319ed77aae4a64cba3410029b6341dafd73d4a94
Main Dev <dev@carinaos.com>
2026-01-09 11:41:19 -0600

    Fixed spacing in IsJsonTest

--- src/infrastructure/http/response/validators/IsJson.test.ts
+++ src/infrastructure/http/response/validators/IsJson.test.ts
@@ -8,12 +8,14 @@ describe('IsJson', () => {
     test('should return true for valid JSON response', async () => {
         const validator = new IsJson();
         const response = new HttpResponse(new Response(JSON.stringify({ foo: "bar" })));
+
         expect(await validator.isValid(response)).toBe(true);
     });
 
     test('should return false for invalid JSON response', async () => {
         const validator = new IsJson();
         const response = new HttpResponse(new Response("not json"));
+
         expect(await validator.isValid(response)).toBe(false);
     });
 });
