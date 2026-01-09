30488a7f7a3d6e54e94a3af44d78cde733956f58
Main Dev <dev@carinaos.com>
2026-01-09 11:40:59 -0600

    Fixed spacing in HasRequiredPropertiesTest

--- src/infrastructure/http/response/validators/HasRequiredProperties.test.ts
+++ src/infrastructure/http/response/validators/HasRequiredProperties.test.ts
@@ -8,12 +8,14 @@ describe('HasRequiredProperties', () => {
     test('should return true if required properties are present', async () => {
         const validator = new HasRequiredProperties(["foo"]);
         const response = new HttpResponse(new Response(JSON.stringify({ foo: "bar" })));
+
         expect(await validator.isValid(response)).toBe(true);
     });
 
     test('should return false if required properties are missing', async () => {
         const validator = new HasRequiredProperties(["foo"]);
         const response = new HttpResponse(new Response(JSON.stringify({ bar: "baz" })));
+
         expect(await validator.isValid(response)).toBe(false);
     });
 });
